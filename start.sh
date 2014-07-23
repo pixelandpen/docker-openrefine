2b122733d199def35a520f28457c3e841eaa2771
Stefano Parmesan <s.parmesan@gmail.com>
2014-07-23 14:51:23 +0200

    Update start.sh
    the default value for `REFINE_MEMORY` is now set to the `60%` of the total memory of the machine

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ -z "$REFINE_MEMORY" ] ; then
-    REFINE_MEMORY="1024M"
+    TOTAL_MEMORY=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+    REFINE_MEMORY=$(( $TOTAL_MEMORY * 6 / 10 ))
 fi
 
 OpenRefine/refine -i 0.0.0.0 -d /mnt/refine -m $REFINE_MEMORY
