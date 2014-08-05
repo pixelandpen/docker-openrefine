a41e688484efb50839da405264e38f07316cf0f1
Stefano Parmesan <s.parmesan@gmail.com>
2014-08-05 08:40:28 +0200

    Update start.sh
    If `REFINE_MEMORY` is not set, the amount of memory given to refine will be `max(60% of the total memory, total memory - 3GB)`

--- start.sh
+++ start.sh
@@ -2,7 +2,12 @@
 
 if [ -z "$REFINE_MEMORY" ] ; then
     TOTAL_MEMORY=`free -b | grep Mem | awk '{print $2}'`
+    MIN_REFINE_MEMORY=$(( $TOTAL_MEMORY - 3 * 1024 * 1024 * 1024 ))
     REFINE_MEMORY=$(( $TOTAL_MEMORY * 6 / 10 ))
+
+    if [ "$REFINE_MEMORY" -lt "$MIN_REFINE_MEMORY" ]; then
+        REFINE_MEMORY="$MIN_REFINE_MEMORY"
+    fi
 fi
 
 exec OpenRefine/refine -i 0.0.0.0 -d /mnt/refine -m $REFINE_MEMORY
